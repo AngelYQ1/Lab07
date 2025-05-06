0012c1d49be551b2d8a8d806fc13dc1a4373624b
Diego Zahid Quispe Quispe <diego.quispe.qui@tecsup.edu.pe>
2025-05-06 14:13:56 -0500

    Cambios en el main para ejecutar la app

--- app/src/main/java/com/example/datossinmvvm_/MainActivity.kt
+++ app/src/main/java/com/example/datossinmvvm_/MainActivity.kt
@@ -16,15 +16,12 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            DatosSinMVVMTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+            ScreenUser()
                 }
             }
         }
-    }
-}
+
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
